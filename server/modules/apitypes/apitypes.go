92eae1f30dead58a8a534a38cc990b7749da203c
Mortvvnutri <93926034+mortvvnutri@users.noreply.github.com>
2024-04-07 16:38:22 +0300

    Added types for future integration with OpenWeatherMap

--- server/modules/apitypes/apitypes.go
+++ server/modules/apitypes/apitypes.go
@@ -36,6 +36,11 @@ type API_obj struct {
 
 	Category   *Category_Obj   `json:"category,omitempty"`
 	Categories *[]Category_Obj `json:"categories,omitempty"`
+
+	PosReq  *PosReq_Obj   `json:"pos_req,omitempty"`
+	PosReqs *[]PosReq_Obj `json:"pos_reqs,omitempty"`
+
+	Weather *OWM_Weather `json:"weather,omitempty"`
 }
 
 type User_Obj struct {
@@ -92,15 +97,16 @@ type Feedback_Obj struct {
 }
 
 type Place_Obj struct {
-	Id          *int       `json:"id,omitempty"`
-	Name        *string    `json:"name,omitempty"`
-	Description *string    `json:"description,omitempty"`
-	Lat         *float64   `json:"lat,omitempty"`
-	Long        *float64   `json:"long,omitempty"`
-	CategoryId  *int       `json:"category_id,omitempty"`
-	Meta        *string    `json:"meta,omitempty"`
-	CreatedAt   *time.Time `json:"created_at,omitempty"`
-	UpdatedAt   *time.Time `json:"updated_at,omitempty"`
+	Id          *int           `json:"id,omitempty"`
+	Name        *string        `json:"name,omitempty"`
+	Description *string        `json:"description,omitempty"`
+	Lat         *float64       `json:"lat,omitempty"`
+	Long        *float64       `json:"long,omitempty"`
+	CategoryId  *int           `json:"category_id,omitempty"`
+	POptions    *pq.Int32Array `json:"p_options,omitempty"`
+	Meta        *string        `json:"meta,omitempty"`
+	CreatedAt   *time.Time     `json:"created_at,omitempty"`
+	UpdatedAt   *time.Time     `json:"updated_at,omitempty"`
 }
 
 type Category_Obj struct {
@@ -121,4 +127,99 @@ type CustomPlace_Obj struct {
 	Meta      *string    `json:"meta,omitempty"`
 	CreatedAt *time.Time `json:"created_at,omitempty"`
 	UpdatedAt *time.Time `json:"updated_at,omitempty"`
+}
+
+type PosReq_Obj struct {
+	MyLat  *float64       `json:"my_lat,omitempty"`
+	MyLong *float64       `json:"my_long,omitempty"`
+	Radius *float64       `json:"radius,omitempty"`
+	Cats   *pq.Int32Array `json:"cats,omitempty"`
+}
+
+// OpenWeatherMap
+
+type OWM_Weather struct {
+	Coord      *OWM_W_Coord         `json:"coord,omitempty"`
+	Weather    *OWM_W_Weather       `json:"weather,omitempty"`
+	Base       *string              `json:"base,omitempty"`
+	Main       *OWM_W_Main          `json:"main,omitempty"`
+	Visibility *float32             `json:"visibility,omitempty"`
+	Wind       *OWM_W_Wind          `json:"wind,omitempty"`
+	Clouds     *OWM_W_Clouds        `json:"clouds,omitempty"`
+	Rain       *OWM_W_Precipitation `json:"rain,omitempty"`
+	Snow       *OWM_W_Precipitation `json:"snow,omitempty"`
+	Dt         *int64               `json:"dt,omitempty"`
+	Sys        *OWM_W_Sys           `json:"sys,omitempty"`
+	Timezone   *int                 `json:"timezone,omitempty"`
+	Id         *int64               `json:"id,omitempty"`
+	Name       *string              `json:"name,omitempty"`
+	Cod        *int64               `json:"cod,omitempty"`
+}
+
+type OWM_W_Sys struct {
+	Type    *int64  `json:"type,omitempty"`
+	Id      *int64  `json:"id,omitempty"`
+	Country *string `json:"country,omitempty"`
+	Sunrise *int64  `json:"sunrise,omitempty"`
+	Sunset  *int64  `json:"sunset,omitempty"`
+}
+
+type OWM_W_Clouds struct {
+	All *int `json:"all,omitempty"`
+}
+
+type OWM_W_Precipitation struct {
+	H1  *float32 `json:"1h,omitempty"`
+	H2  *float32 `json:"2h,omitempty"`
+	H3  *float32 `json:"3h,omitempty"`
+	H4  *float32 `json:"4h,omitempty"`
+	H5  *float32 `json:"5h,omitempty"`
+	H6  *float32 `json:"6h,omitempty"`
+	H7  *float32 `json:"7h,omitempty"`
+	H8  *float32 `json:"8h,omitempty"`
+	H9  *float32 `json:"9h,omitempty"`
+	H10 *float32 `json:"10h,omitempty"`
+	H11 *float32 `json:"11h,omitempty"`
+	H12 *float32 `json:"12h,omitempty"`
+	H13 *float32 `json:"13h,omitempty"`
+	H14 *float32 `json:"14h,omitempty"`
+	H15 *float32 `json:"15h,omitempty"`
+	H16 *float32 `json:"16h,omitempty"`
+	H17 *float32 `json:"17h,omitempty"`
+	H18 *float32 `json:"18h,omitempty"`
+	H19 *float32 `json:"19h,omitempty"`
+	H20 *float32 `json:"20h,omitempty"`
+	H21 *float32 `json:"21h,omitempty"`
+	H22 *float32 `json:"22h,omitempty"`
+	H23 *float32 `json:"23h,omitempty"`
+	H24 *float32 `json:"24h,omitempty"`
+}
+
+type OWM_W_Wind struct {
+	Speed *float32 `json:"speed,omitempty"`
+	Deg   *float32 `json:"deg,omitempty"`
+	Gust  *float32 `json:"gust,omitempty"`
+}
+
+type OWM_W_Main struct {
+	Temp        *float32 `json:"temp,omitempty"`
+	FeelsLike   *float32 `json:"feels_like,omitempty"`
+	Pressure    *float32 `json:"pressure,omitempty"`
+	Humidity    *float32 `json:"humidity,omitempty"`
+	TempMin     *float32 `json:"temp_min,omitempty"`
+	TempMax     *float32 `json:"temp_max,omitempty"`
+	SeaLevel    *float32 `json:"sea_level,omitempty"`
+	GroundLevel *float32 `json:"grnd_level,omitempty"`
+}
+
+type OWM_W_Weather struct {
+	Id          *int64  `json:"id,omitempty"`
+	Main        *string `json:"main,omitempty"`
+	Description *string `json:"description,omitempty"`
+	Icon        *string `json:"icon,omitempty"`
+}
+
+type OWM_W_Coord struct {
+	Lat *float64 `json:"lat,omitempty"`
+	Lon *float64 `json:"lon,omitempty"`
 }
